1aa669fd2b6c5f0d40f0855476cc52cec69beace
Mohammed Ahmed <2149869@talnet.nl>
2022-02-01 12:17:38 +0100

    Taak02: Loops

---
+++ 2-Loops/taak02/for-loop.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html>
+<body>
+
+<?php
+
+for($leeftijd = 0; $leeftijd < 18; $leeftijd++){
+    echo"Ik ben $leeftijd jaar, dus ik mag nog niet stemmen. <br />";
+}
+echo"Ik ben $leeftijd, dus ik heb stemrecht!";
+
+?>
+
+</body>
+</html>
