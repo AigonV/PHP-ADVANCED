faae42f8b548fa1307509ea51d235cbbe6263c54
Mohammed Ahmed <2149869@talnet.nl>
2022-02-01 11:40:22 +0100

    Taak-2: Arrays

---
+++ 1-Arrays/taak02/index.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html>
+<body>
+
+<?php
+
+$persoon["Voornaam"] = "Mohammed";
+$persoon["Achternaam"] = "Ahmed";
+$persoon["Leeftijd"] = "17";
+$persoon["Opleiding"] = "Software Developer";
+
+echo $persoon['Voornaam']. " " . $persoon['Achternaam']. " is " .  $persoon['Leeftijd']. " jaar oud" .  " en is student aan het ROC van Amsterdam aan de opleiding " . $persoon['Opleiding'] ;
+
+?>
+
+</body>
+</html>
