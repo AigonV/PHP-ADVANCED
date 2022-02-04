fb4e045aa3122a788f4c46851fffa2ec25cce142
Mohammed Ahmed <2149869@talnet.nl>
2022-02-04 10:47:09 +0100

    Taak01: Form

---
+++ 3-Forms/taak01/mijngegevens.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html>
+
+<?php 
+
+echo"Voornaam: ".$_POST["voornaam"];
+echo"<br/>";
+echo"Achternaam: ".$_POST["achternaam"];
+echo"<br/>";
+echo"Klas: ".$_POST["klas"];
+echo"<br/>";
+echo"Leeftijd: ".$_POST["leeftijd"];
+echo"<br/>";
+echo"Adres: ".$_POST["adres"];
+echo"<br/>";
+echo"Plaatsnaam: ".$_POST["plaatsnaam"];
+echo"<br/>";
+echo"Favoriete muziekband: ".$_POST["favmuziek"];
+?>
+
+</body>
+</html>
