d39fb93edcd05909b6d2e38a9d91fb1279c7b1f5
Mohammed Ahmed <2149869@talnet.nl>
2022-02-10 14:14:38 +0100

    Taak02: Functies

--- 4-Functions/taak02/functies-2.php
+++ 4-Functions/taak02/functies-2.php
@@ -17,7 +17,12 @@ function rekenUit($getal, $getal2, $getal3){
 
 }
 
-echo"Als ik 5, 3 en 8 bij elkaar optel, krijg ik ". rekenUit(5, 3, 8);
+echo"Als ik 79, 51 en 22 bij elkaar optel, krijg ik ". rekenUit(79, 51, 22);
+echo"<br/>";
+echo"Als ik 45, 2 en 12 bij elkaar optel, krijg ik ". rekenUit(45, 2, 12);
+echo"<br/>";
+echo"Als ik 34, 55 en 642 bij elkaar optel, krijg ik ". rekenUit(34, 55, 642);
+echo"<br/>";
 ?>
 
 </body>
