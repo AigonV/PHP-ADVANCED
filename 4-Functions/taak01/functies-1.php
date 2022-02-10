bc698e9a2a7501e6e3ba0410569812db9e86246f
Mohammed Ahmed <2149869@talnet.nl>
2022-02-10 13:33:48 +0100

    Taak01: Functies

---
+++ 4-Functions/taak01/functies-1.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html>
+
+<?php 
+
+function schrijfKleur($kleur){
+    echo"<p style=color:$kleur>Dit is de kleur: $kleur</p>";
+}
+
+schrijfKleur("red");
+schrijfKleur("green");
+schrijfKleur("yellow");
+schrijfKleur("blue");
+
+?>
+
+</body>
+</html>
